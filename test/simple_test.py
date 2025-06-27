9af4a341d20bfb217340fbec8791c0df3fc98ea2
Eirik Solberg <eirsolb@stud.ntnu.no>
2025-06-27 08:48:08 +0200

    test: simple test for import from everest_optimizers

--- test/simple_test.py
+++ test/simple_test.py
@@ -1,7 +1,7 @@
-import simple_test
+import everest_optimizers
 
 def test_add():
     assert 1 + 1 == 2
 
 def test_test_optpp():
-    assert not None
+    assert everest_optimizers.test_optpp() == "Everest Optimizers OptQNewton binding compilation successful!"
