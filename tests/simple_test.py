1db9d13d3a289194c8da910edfbaef898f891115
Eirik Solberg <eirsolb@stud.ntnu.no>
2025-06-27 12:23:20 +0200

    style: run pre-commit on all files

--- tests/simple_test.py
+++ tests/simple_test.py
@@ -1,7 +1,12 @@
 import everest_optimizers
 
+
 def test_add():
     assert 1 + 1 == 2
 
+
 def test_test_optpp():
-    assert everest_optimizers.test_optpp() == "Everest Optimizers OptQNewton binding compilation successful!"
+    assert (
+        everest_optimizers.test_optpp()
+        == "Everest Optimizers OptQNewton binding compilation successful!"
+    )
