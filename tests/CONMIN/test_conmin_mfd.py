95b51a481a0c9837ec5cd12a50496cac9e8fdfe6
Eirik Solberg <eiriksol03@gmail.com>
2025-08-04 12:29:09 +0200

    run ruff on files

--- tests/CONMIN/test_conmin_mfd.py
+++ tests/CONMIN/test_conmin_mfd.py
@@ -2,19 +2,39 @@ from everest_optimizers.minimize import minimize
 import numpy as np
 import pytest
 
+
 def test_rosen_suzuki_constraints_satisfied():
-    
     def obj(x):
         return (
-            x[0]**2 - 5*x[0] +
-            x[1]**2 - 5*x[1] +
-            2*x[2]**2 - 21*x[2] +
-            x[3]**2 + 7*x[3] + 50
+            x[0] ** 2
+            - 5 * x[0]
+            + x[1] ** 2
+            - 5 * x[1]
+            + 2 * x[2] ** 2
+            - 21 * x[2]
+            + x[3] ** 2
+            + 7 * x[3]
+            + 50
         )
 
-    def constraint1(x): return x[0]**2 + x[0] + x[1]**2 - x[1] + x[2]**2 + x[2] + x[3]**2 - x[3] - 8
-    def constraint2(x): return x[0]**2 - x[0] + 2*x[1]**2 + x[2]**2 + 2*x[3]**2 - x[3] - 10
-    def constraint3(x): return 2*x[0]**2 + 2*x[0] + x[1]**2 - x[1] + x[2]**2 - x[3] - 5
+    def constraint1(x):
+        return (
+            x[0] ** 2
+            + x[0]
+            + x[1] ** 2
+            - x[1]
+            + x[2] ** 2
+            + x[2]
+            + x[3] ** 2
+            - x[3]
+            - 8
+        )
+
+    def constraint2(x):
+        return x[0] ** 2 - x[0] + 2 * x[1] ** 2 + x[2] ** 2 + 2 * x[3] ** 2 - x[3] - 10
+
+    def constraint3(x):
+        return 2 * x[0] ** 2 + 2 * x[0] + x[1] ** 2 - x[1] + x[2] ** 2 - x[3] - 5
 
     x0 = np.array([1.0, 1.0, 1.0, 1.0])
 
@@ -32,7 +52,7 @@ def test_rosen_suzuki_constraints_satisfied():
         method="conmin_mfd",
         bounds=bounds,
         constraints=constraints,
-        options={"ITMAX": 100}
+        options={"ITMAX": 100},
     )
 
     # Expected solution
@@ -43,24 +63,48 @@ def test_rosen_suzuki_constraints_satisfied():
     assert np.allclose(result.x, expected_x, atol=1e-2)
     assert np.isclose(result.fun, expected_fun, atol=1e-2)
 
-@pytest.mark.parametrize("x0", [
-    np.array([-4.0, -4.0, -4.0, -4.0]),
-    np.array([4.0, 4.0, 4.0, 4.0]),
-    np.array([0.0, 0.0, 0.0, 0.0]),
-    np.array([0.1, 0.9, 2.1, -1.2])
-])
+
+@pytest.mark.parametrize(
+    "x0",
+    [
+        np.array([-4.0, -4.0, -4.0, -4.0]),
+        np.array([4.0, 4.0, 4.0, 4.0]),
+        np.array([0.0, 0.0, 0.0, 0.0]),
+        np.array([0.1, 0.9, 2.1, -1.2]),
+    ],
+)
 def test_rosen_suzuki_multiple_initial_guesses(x0):
     def obj(x):
         return (
-            x[0]**2 - 5*x[0] +
-            x[1]**2 - 5*x[1] +
-            2*x[2]**2 - 21*x[2] +
-            x[3]**2 + 7*x[3] + 50
+            x[0] ** 2
+            - 5 * x[0]
+            + x[1] ** 2
+            - 5 * x[1]
+            + 2 * x[2] ** 2
+            - 21 * x[2]
+            + x[3] ** 2
+            + 7 * x[3]
+            + 50
         )
 
-    def constraint1(x): return x[0]**2 + x[0] + x[1]**2 - x[1] + x[2]**2 + x[2] + x[3]**2 - x[3] - 8
-    def constraint2(x): return x[0]**2 - x[0] + 2*x[1]**2 + x[2]**2 + 2*x[3]**2 - x[3] - 10
-    def constraint3(x): return 2*x[0]**2 + 2*x[0] + x[1]**2 - x[1] + x[2]**2 - x[3] - 5
+    def constraint1(x):
+        return (
+            x[0] ** 2
+            + x[0]
+            + x[1] ** 2
+            - x[1]
+            + x[2] ** 2
+            + x[2]
+            + x[3] ** 2
+            - x[3]
+            - 8
+        )
+
+    def constraint2(x):
+        return x[0] ** 2 - x[0] + 2 * x[1] ** 2 + x[2] ** 2 + 2 * x[3] ** 2 - x[3] - 10
+
+    def constraint3(x):
+        return 2 * x[0] ** 2 + 2 * x[0] + x[1] ** 2 - x[1] + x[2] ** 2 - x[3] - 5
 
     constraints = [
         {"type": "ineq", "fun": constraint1},
@@ -75,7 +119,7 @@ def test_rosen_suzuki_multiple_initial_guesses(x0):
         method="conmin_mfd",
         bounds=bounds,
         constraints=constraints,
-        options={"ITMAX": 100}
+        options={"ITMAX": 100},
     )
 
     expected_x = np.array([0.0, 1.0, 2.0, -1.0])
