999987e2ea57cb3f820728fd9f048bf7a6e13901
Håvard Berland <havb@equinor.com>
2026-05-21 14:49:27 +0200

    Move everest integration testing into its own bash function
    Facilitates interactive debugging of test code

--- ci/testkomodo.sh
+++ ci/testkomodo.sh
@@ -2,11 +2,7 @@ install_test_dependencies () {
     pip install pytest
 }
 
-start_tests () {
-    set -e
-    pytest
-
-    # Integration test with everest
+start_everest_integration_tests () {
     rm -rf math_func
     git clone --depth 1 https://github.com/equinor/ert __ert
     cp -a __ert/test-data/everest/math_func .
@@ -16,4 +12,10 @@ start_tests () {
     sed -i  's/optpp_q_newton/bcq_newton/' config_minimal.yml
     everest run config_minimal.yml
     popd
+}
+
+start_tests () {
+    set -e
+    pytest
+    start_everest_integration_tests
 }
