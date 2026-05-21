889c15842b1c3a0668084d6a5b9db4afaa7eb2a1
Håvard Berland <havb@equinor.com>
2026-05-21 14:54:13 +0200

    Add integration test of q_nips algorithm

--- ci/testkomodo.sh
+++ ci/testkomodo.sh
@@ -9,8 +9,13 @@ start_everest_integration_tests () {
     rm -rf __ert
 
     pushd math_func
+
     sed -i  's/optpp_q_newton/bcq_newton/' config_minimal.yml
     everest run config_minimal.yml
+
+    sed -i  's/optpp_q_newton/q_nips/' config_advanced.yml
+    everest run config_advanced.yml
+
     popd
 }
 
